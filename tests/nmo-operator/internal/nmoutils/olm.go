3a0377cf78c38786dc91de4d6899a7eb2ca80e9f
JonahSussman <sussmanjonah@gmail.com>
2026-09-01 14:04:26 -0400

    Split NMO upgrade test into operator-upgrade and cluster-upgrade specs
    Signed-off-by: Jonah Sussman <jsussman@redhat.com>

--- tests/nmo-operator/internal/nmoutils/olm.go
+++ tests/nmo-operator/internal/nmoutils/olm.go
@@ -56,7 +56,9 @@ func GetNMOControllerImage(apiClient *clients.Settings) (string, error) {
 	)
 }
 
-// CleanupUpgradeResources removes the Subscription created during the upgrade test.
+// CleanupUpgradeResources removes the Subscription, CSV, and InstallPlan
+// created during the upgrade test.
 func CleanupUpgradeResources(apiClient *clients.Settings, logf func(string, ...interface{})) {
-	helpers.DeleteSubscription(apiClient, nmoparams.UpgradeSubName, medik8sparams.OperatorNs, logf)
+	helpers.CleanupSubscriptionAndCSV(
+		apiClient, nmoparams.UpgradeSubName, medik8sparams.OperatorPackage, medik8sparams.OperatorNs, logf)
 }
